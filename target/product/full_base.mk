36e4414145c73f4dfa8369c4dcb167102aa838e5
Chet Kener <Cl3Kener@gmail.com>
2014-12-12 12:54:41 -0500

    Kill Bloat with Fire from Rome!
    Signed-off-by: Chet Kener <Cl3Kener@gmail.com>
    
    Conflicts:
    	target/product/full_base.mk
    
    Prohibited bloat

--- target/product/full_base.mk
+++ target/product/full_base.mk
@@ -28,15 +28,7 @@ PRODUCT_PACKAGES := \
     WAPPushManager
 
 PRODUCT_PACKAGES += \
-    Galaxy4 \
-    HoloSpiralWallpaper \
-    LiveWallpapers \
-    LiveWallpapersPicker \
-    MagicSmokeWallpapers \
-    NoiseField \
-    PhaseBeam \
-    VisualizationWallpapers \
-    PhotoTable
+    LiveWallpapersPicker
 
 # Additional settings used in all AOSP builds
 PRODUCT_PROPERTY_OVERRIDES := \
@@ -52,9 +44,6 @@ PRODUCT_AAPT_CONFIG := normal hdpi xhdpi xxhdpi
 
 # Get some sounds
 $(call inherit-product-if-exists, frameworks/base/data/sounds/AudioPackage13_48.mk)
-
-# Get the TTS language packs
-$(call inherit-product-if-exists, external/svox/pico/lang/all_pico_languages.mk)
 
 # Get a list of languages.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/locales_full.mk)
