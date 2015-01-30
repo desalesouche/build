3a055799cf2a72cc5f3d8ce3e3b44c196625e7d3
GalaticStryder <icarohoff@gmail.com>
2015-01-30 11:01:13 -0200

    Update Host TC && Neonize

--- core/clang/arm.mk
+++ core/clang/arm.mk
@@ -6,6 +6,12 @@ CLANG_CONFIG_arm_EXTRA_ASFLAGS := \
 CLANG_CONFIG_arm_EXTRA_CFLAGS := \
   -no-integrated-as
 
+ifneq (,$(filter krait,$(TARGET_$(combo_2nd_arch_prefix)CPU_VARIANT)))
+  # Android's clang support's krait as a CPU whereas GCC doesn't. Specify
+  # -mcpu here rather than the more normal core/combo/arch/arm/armv7-a-neon.mk.
+  CLANG_CONFIG_arm_EXTRA_CFLAGS += $(call cc-option,-mcpu=swift,-mcpu=cortex-a15)
+endif
+
 CLANG_CONFIG_arm_EXTRA_CPPFLAGS := \
   -no-integrated-as
 
@@ -22,11 +28,20 @@ CLANG_CONFIG_arm_UNKNOWN_CFLAGS := \
   -fno-builtin-sin \
   -fno-strict-volatile-bitfields \
   -fno-align-jumps \
-  -Wa,--noexecstack
+  -Wa,--noexecstack \
+  -Wno-unused-local-typedefs \
+  -fpredictive-commoning \
+  -ftree-loop-distribute-patterns \
+  -fvect-cost-model \
+  -ftree-partial-pre \
+  -fipa-cp-clone
 
 define subst-clang-incompatible-arm-flags
   $(subst -march=armv5te,-march=armv5t,\
   $(subst -march=armv5e,-march=armv5,\
-  $(subst -mcpu=cortex-a15,-march=armv7-a,\
-  $(1))))
+  $(subst -mfpu=neon-vfp3,-mfpu=neon,\
+  $(subst -mfpu=neon-vfpv3,-mfpu=neon,\
+  $(subst -mfpu=neon-vfp4,-mfpu=neon,\
+  $(subst -mfpu=neon-vfpv4,-mfpu=neon,\
+  $(1)))))))
 endef
