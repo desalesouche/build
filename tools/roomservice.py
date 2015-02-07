83d2d05b7f745b29de0ebc52d73f632e6a9ae8df
GalaticStryder <icarohoff@gmail.com>
2015-02-07 15:13:16 -0200

    Revert "Changing dependencies name you need to change this"
    This reverts commit 123325813b6ded8fbe2b109a2a874b663f07f21d.

--- tools/roomservice.py
+++ tools/roomservice.py
@@ -35,16 +35,16 @@ except ImportError:
 
 # Config
 # set this to the default remote to use in repo
-default_rem = "ss"
+default_rem = "github"
 # set this to the default revision to use (branch/tag name)
-default_rev = "ss5.0"
+default_rev = "lollipop"
 # set this to the remote that you use for projects from your team repos
 # example fetch="https://github.com/omnirom"
 default_team_rem = "github"
 # this shouldn't change unless google makes changes
 local_manifest_dir = ".repo/local_manifests"
 # change this to your name on github (or equivalent hosting)
-android_team = "SaberSunset"
+android_team = "AOSPA-legacy"
 
 
 def check_repo_exists(git_data):
@@ -226,7 +226,7 @@ def parse_device_from_folder(device):
 
 
 def parse_dependency_file(location):
-    dep_file = "ss.dependencies"
+    dep_file = "pa.dependencies"
     dep_location = '/'.join([location, dep_file])
     if not os.path.isfile(dep_location):
         print("WARNING: %s file not found" % dep_location)
