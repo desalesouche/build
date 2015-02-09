ce44a4b3ab6abc57eb1599ca91450cdbaf242bb4
GalaticStryder <icarohoff@gmail.com>
2015-02-09 18:12:35 -0200

    Revert "core: SaberMod Strict-aliasing"
    This reverts commit dfaf77516a04a69f45806f4b602b1315e615d170.

--- core/binary.mk
+++ core/binary.mk
@@ -101,10 +101,6 @@ ifeq ($(LIQUIFY),true)
 include $(BUILD_SYSTEM)/graphite.mk
 endif
 
-ifeq ($(LIQUIFY),true)
-include $(BUILD_SYSTEM)/strict.mk
-endif
-
 # The following LOCAL_ variables will be modified in this file.
 # Because the same LOCAL_ variables may be used to define modules for both 1st arch and 2nd arch,
 # we can't modify them in place.
