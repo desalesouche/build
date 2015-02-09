XileForce/Onyx-Build

unbreak strict-aliasing:  more modules may need to be forced disabled
Signed-off-by: Paul Beeler <pbeeler80@gmail.com>

Conflicts:
	core/strict.mk

Change-Id: If8ee3e5664f5540817b50c39c51fe00ba5f6534d

core/strict.mk
@@ -70,7 +70,32 @@ LOCAL_DISABLE_STRICT := \
     camera.msm8084 \
 	libc_nomalloc
 
-ifneq (1,$(words $(filter $(LOCAL_DISABLE_STRICT), $(LOCAL_MODULE))))
+# Force no strict-aliasing on some modules
+LOCAL_FORCE_DISABLE_STRICT := \
+	libziparchive-host \
+	libc_bionic \
+	libc_dns \
+	libziparchive
+
+ifeq (1,$(words $(filter $(LOCAL_FORCE_DISABLE_STRICT),$(LOCAL_MODULE))))
+ifdef LOCAL_CONLYFLAGS
+LOCAL_CONLYFLAGS += \
+	-fno-strict-aliasing
+else
+LOCAL_CONLYFLAGS := \
+	-fno-strict-aliasing
+endif
+ifdef LOCAL_CPPFLAGS
+LOCAL_CPPFLAGS += \
+	-fno-strict-aliasing
+else
+LOCAL_CPPFLAGS := \
+	-fno-strict-aliasing
+endif
+endif
+
+# Test local module disabled list.
+ifneq (1,$(words $(filter $(LOCAL_DISABLE_STRICT),$(LOCAL_MODULE))))
 ifdef LOCAL_CONLYFLAGS
 LOCAL_CONLYFLAGS += \
 	-fstrict-aliasing \

desalesouche/build

core/strict.mk
@@ -68,7 +68,32 @@ LOCAL_DISABLE_STRICT := \
 	libc \
 	libc_nomalloc
 
-ifneq (1,$(words $(filter $(LOCAL_DISABLE_STRICT), $(LOCAL_MODULE))))
+# Force no strict-aliasing on some modules
+LOCAL_FORCE_DISABLE_STRICT := \
+	libziparchive-host \
+	libc_bionic \
+	libc_dns \
+	libziparchive
+
+ifeq (1,$(words $(filter $(LOCAL_FORCE_DISABLE_STRICT),$(LOCAL_MODULE))))
+ifndef LOCAL_CONLYFLAGS
+LOCAL_CONLYFLAGS += \
+	-fno-strict-aliasing
+else
+LOCAL_CONLYFLAGS := \
+	-fno-strict-aliasing
+endif
+ifdef LOCAL_CPPFLAGS
+LOCAL_CPPFLAGS += \
+	-fno-strict-aliasing
+else
+LOCAL_CPPFLAGS := \
+	-fno-strict-aliasing
+endif
+endif
+
+# Test local module disabled list.
+ifneq (1,$(words $(filter $(LOCAL_DISABLE_STRICT),$(LOCAL_MODULE))))
 ifndef LOCAL_CONLYFLAGS
 LOCAL_CONLYFLAGS += \
 	-fstrict-aliasing \