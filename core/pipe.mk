Team-OSE/android_build

Introduce Modular TARGET_USE_PIPE
* Allow for -pipe to be used. This flag actually has no effect on the generated code, but it makes the compilation process faster. It tells the compiler to use pipes instead of temporary files during the different stages of compilation, which uses more memory. On systems with low memory, GCC might get killed. In that case, do not use this flag.

* From my personal experience, compilation time was ~5 minutes less using pipes on a "make -j8" build on my personal computer.  Times may improvement more when doing like a make -j16 or -j24 on a build server with lots of RAM. Also note that this may not speed things up on a SSD hard drive due to the faster write speeds, I only tested this on a regular hard drive.

Signed-off-by: Chet Kener <Cl3Kener@gmail.com>

Conflicts:
	core/binary.mk

core/pipe.mk
@@ -1,0 +1,38 @@
+# Copyright (C) 2015 The SaberMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+LOCAL_DISABLE_PIPE := \
+	libc_dns \
+	libc_tzcode \
+	bluetooth.default
+
+ifneq (1,$(words $(filter $(LOCAL_DISABLE_PIPE), $(LOCAL_MODULE))))
+ifdef LOCAL_CONLYFLAGS
+LOCAL_CONLYFLAGS += \
+	-pipe
+else
+LOCAL_CONLYFLAGS := \
+	-pipe
+endif
+
+ifdef LOCAL_CPPFLAGS
+LOCAL_CPPFLAGS += \
+	-pipe
+else
+LOCAL_CPPFLAGS := \
+	-pipe
+endif
+endif
+#####

desalesouche/build

core/pipe.mk
@@ -1,0 +1,38 @@
+# Copyright (C) 2015 The SaberMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+LOCAL_DISABLE_PIPE := \
+	libc_dns \
+	libc_tzcode \
+	bluetooth.default
+
+ifneq (1,$(words $(filter $(LOCAL_DISABLE_PIPE), $(LOCAL_MODULE))))
+ifndef LOCAL_CONLYFLAGS
+LOCAL_CONLYFLAGS += \
+	-pipe
+else
+LOCAL_CONLYFLAGS := \
+	-pipe
+endif
+
+ifdef LOCAL_CPPFLAGS
+LOCAL_CPPFLAGS += \
+	-pipe
+else
+LOCAL_CPPFLAGS := \
+	-pipe
+endif
+endif
+#####